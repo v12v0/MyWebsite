648bdf2cfe1ebbe43e29735bbdc4f4596dfd07e7
Vi Vo <aaronvo118@gmail.com>
2022-01-12 11:47:23 -0600

    fixed footer

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.getElementById("contact").addEventListener("click", function() {
-    alert(` Feel free to reach out for anything! 😄
-    Email: vi.vo@utexas.edu 
-    Linkedin: https://www.linkedin.com/in/v12v0/`
-        );});
+    alert(`Feel free to reach out for anything! 😄
+        Email: vi.vo@utexas.edu 
+        Linkedin: https://www.linkedin.com/in/v12v0/`
+);});
+
+
